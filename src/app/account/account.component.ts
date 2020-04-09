0c11abe5d24d71d0ef9a3268900e757b21358961
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-09 19:16:15 +0200

    Injected Logging Service into Components

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,18 +1,23 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Account, AccountStatus } from '../shared/account.model';
+import { LoggingService } from '../shared/logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: Account;
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<AccountStatus>();
 
+  constructor(private loggingService: LoggingService) {
+  }
+
   onSetTo(status: string) {
     this.statusChanged.emit(new AccountStatus(this.id, status));
-    console.log('A server status changed, new status: ' + status);
+    this.loggingService.logStatusChanged(status);
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,17 +1,22 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
 import { Account } from '../shared/account.model';
+import { LoggingService } from '../shared/logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<Account>();
 
+  constructor(private loggingService: LoggingService) {
+  }
+
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit(new Account(accountName, accountStatus));
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.loggingService.logStatusChanged(accountStatus);
   }
 }
